745acc31e40676a73fb4c05d954446a5e0ad64ee
Aliya Safina <ktotamy@gmail.com>
2020-08-06 20:23:11 +0300

    add build

---
+++ frontend/build/precache-manifest.ffcad00fec5e5eb09ea300091f5e42b1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67fa746fe9f1e71869004b880fb71ef7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5befb428cc7a9087e4e7",
+    "url": "/static/css/main.fa6b7298.chunk.css"
+  },
+  {
+    "revision": "ae9d3868fa6116182ceb",
+    "url": "/static/js/2.f62e0965.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.f62e0965.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5befb428cc7a9087e4e7",
+    "url": "/static/js/main.4f144edb.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "c0ae22e4c1b3e3a1df72387c558930cb",
+    "url": "/static/media/Museocyrl500.c0ae22e4.woff2"
+  },
+  {
+    "revision": "daec729b207d83c2c9c2681a5152160d",
+    "url": "/static/media/Museocyrl500.daec729b.ttf"
+  },
+  {
+    "revision": "ddd56559feabc02ebb2339652e460a35",
+    "url": "/static/media/Museocyrl500.ddd56559.woff"
+  },
+  {
+    "revision": "5d23cb75253ca214c8715a65ab8286bd",
+    "url": "/static/media/Museocyrl700.5d23cb75.ttf"
+  },
+  {
+    "revision": "8609fc096370603c2cb31d67efdea388",
+    "url": "/static/media/Museocyrl700.8609fc09.woff"
+  },
+  {
+    "revision": "f4faf3f2b96588a7d414b14f7c8ed1ec",
+    "url": "/static/media/Museocyrl700.f4faf3f2.woff2"
+  },
+  {
+    "revision": "5382936f81485e4e6420a833f5c751da",
+    "url": "/static/media/circe-rounded-400.5382936f.woff"
+  },
+  {
+    "revision": "8f2c78e238a50de44a892db717dee3b1",
+    "url": "/static/media/circe-rounded-400.8f2c78e2.woff2"
+  },
+  {
+    "revision": "b651ece73597aa2b670a09df07ba0794",
+    "url": "/static/media/circe-rounded-400.b651ece7.ttf"
+  }
+]);
